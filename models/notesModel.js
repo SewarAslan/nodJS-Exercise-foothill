ee2142926aa3d54868f92b0cbacdd98b895af781
SewarAslan <sewaraslan02@gmail.com>
2025-09-09 21:53:59 +0300

    getting the model ready

---
+++ models/notesModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const noteSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  creationDate: {
+    type: Date,
+    default: Date.now,
+  },
+});
+const noteModel = mongoose.model("notes", noteSchema);
+module.exports = noteModel;
